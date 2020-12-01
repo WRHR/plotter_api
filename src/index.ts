eae417a7eaa01324c6873be8e79067b2470f4822
WRHR <wrhreeves@gmail.com>
2020-11-30 22:08:34 -0700

    Create types, create apolloServer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { MikroORM } from "@mikro-orm/core";
 import mikroConfig from "./mikro-orm.config";
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from "type-graphql";
+import MyContext from './types' 
 // import cors from "cors";
 
 const main = async () => {
@@ -15,6 +18,14 @@ const main = async () => {
   //     credentials: true,
   //   })
   // );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers:[],
+      validate:false
+    }),
+    context: ({req, res}): MyContext => ({ em: orm.em, req, res})
+  })
 
   app.listen(4000, () => {
     console.log("Server running on port 4000");
