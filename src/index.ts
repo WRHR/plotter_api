e9a1c8e54eca8ea993ba84e73f281f380b21511d
WRHR <wrhreeves@gmail.com>
2020-11-30 21:21:52 -0700

    Enalbe cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const { MikroORM } = require('@mikro-orm/core')
-import mikroConfig from './mikro-orm.config'
-
+import express from "express";
+import cors from "cors";
+import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm  = await MikroORM.init(mikroConfig)
-  await orm.getMigrator().up()
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
-  const app = express()
-  
-  
-  app.listen(4000, ()=> {
-    console.log('Server running on port 4000')
-  })
-}
+  const app = express();
 
-main()
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+
+  app.listen(4000, () => {
+    console.log("Server running on port 4000");
+  });
+};
+
+main();
