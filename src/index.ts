d61bbde489e7ee8ffa7db0f95f1207c16e83e025
WRHR <wrhreeves@gmail.com>
2020-11-30 21:34:23 -0700

    revert changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import cors from "cors";
 import { MikroORM } from "@mikro-orm/core";
 import mikroConfig from "./mikro-orm.config";
+// import cors from "cors";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -9,12 +9,12 @@ const main = async () => {
 
   const app = express();
 
-  app.use(
-    cors({
-      origin: "http://localhost:3000",
-      credentials: true,
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: "http://localhost:3000",
+  //     credentials: true,
+  //   })
+  // );
 
   app.listen(4000, () => {
     console.log("Server running on port 4000");
